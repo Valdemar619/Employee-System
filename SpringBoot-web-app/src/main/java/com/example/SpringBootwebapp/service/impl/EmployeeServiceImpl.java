597105142deac1283f313b08e59c146e5b6de217
valdemar <vova.malyarchuk2@gmail.com>
2023-08-31 13:44:18 +0300

    done

--- SpringBoot-web-app/src/main/java/com/example/SpringBootwebapp/service/impl/EmployeeServiceImpl.java
+++ SpringBoot-web-app/src/main/java/com/example/SpringBootwebapp/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.example.SpringBootwebapp.model.Employee;
 import com.example.SpringBootwebapp.repository.EmployeeRepository;
 import com.example.SpringBootwebapp.service.EmployeeService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,5 +16,27 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @Override
+    public void saveEmployee(Employee employee) {
+        this.employeeRepository.save(employee);
+    }
+
+    @Override
+        public Employee getEmployeeById(Long id) {
+        Optional<Employee> optional =  employeeRepository.findById(id);
+        Employee employee = null;
+        if (optional.isPresent()) {
+            employee = optional.get();
+        } else {
+            throw new RuntimeException(" Employee not found for id :: " + id);
+        }
+        return employee;
+    }
+
+    @Override
+    public void deleteEmployeeById(Long id) {
+        this.employeeRepository.deleteById(id);
     }
 }
